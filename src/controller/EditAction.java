c4d3aaa140c8b93e7d4cddaf58b5384a30f432ab
jovanamaletin <107765229+jovanamaletin@users.noreply.github.com>
2022-06-21 11:18:02 +0200

    Verzija 2

--- src/controller/EditAction.java
+++ src/controller/EditAction.java
@@ -23,4 +23,38 @@ public class EditAction extends AbstractAction {
         putValue(Action.SMALL_ICON, new ImageIcon("src/icons/edit.png"));
     }
 
- }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JTable table = null;
+        int col; int row;
+        int tabSelected = AppCore.getMainFrame().getContent().getSelectedIndex();
+        if (tabSelected == 0) {
+            table = AppCore.getMainFrame().getTableZaposleni();
+            col = 3;
+        } else {
+            table = AppCore.getMainFrame().getTableSoftveri();
+            col = 0;
+        }
+
+        row = table.getSelectedRow();
+        if (row == -1) {
+            JOptionPane.showMessageDialog(null, "No row selected!", "WARNING", JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+
+        JDialog dialog = null;
+        if (table instanceof TableZaposleni) {
+            String jmbg = table.getModel().getValueAt(row, col).toString();
+            Zaposleni z = AppCore.getDatabase().findZaposleni(jmbg);
+            dialog = new ZaposleniDialog(z);
+        }
+
+        if (table instanceof TableSoftveri) {
+            String naziv = table.getModel().getValueAt(row, col).toString();
+            Softver s = AppCore.getDatabase().findSoftver(naziv);
+            dialog = new SoftverDialog(s);
+        }
+
+        dialog.setVisible(true);
+    }
+}
